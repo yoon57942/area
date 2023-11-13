8d3518bac394f843351f85c7f6aa9e674795885b
yoon57942 <yoon57942@hanyang.ac.kr>
2023-11-13 19:20:11 +0900

    git new

---
+++ MIRU RUST/git_hello/target/debug/deps/git_hello-2c9a730d61ec2d64.d
@@ -0,0 +1,5 @@
+C:\Users\yoon1\MIRU RUST\git_hello\target\debug\deps\git_hello-2c9a730d61ec2d64.rmeta: src\main.rs
+
+C:\Users\yoon1\MIRU RUST\git_hello\target\debug\deps\git_hello-2c9a730d61ec2d64.d: src\main.rs
+
+src\main.rs:
---
+++ MIRU RUST/git_hello/target/debug/deps/git_hello-d319de01645610e1.d
@@ -0,0 +1,5 @@
+C:\Users\yoon1\MIRU RUST\git_hello\target\debug\deps\git_hello-d319de01645610e1.rmeta: src\main.rs
+
+C:\Users\yoon1\MIRU RUST\git_hello\target\debug\deps\git_hello-d319de01645610e1.d: src\main.rs
+
+src\main.rs:
