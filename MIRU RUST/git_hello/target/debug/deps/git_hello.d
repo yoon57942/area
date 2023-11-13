6f95de1c8c2989e9be8a91a0fb81032006aa5943
yoon57942 <yoon57942@hanyang.ac.kr>
2023-11-13 19:55:37 +0900

    print hello mitu

---
+++ MIRU RUST/git_hello/target/debug/deps/git_hello.d
@@ -0,0 +1,5 @@
+C:\Users\yoon1\MIRU RUST\git_hello\target\debug\deps\git_hello.exe: src\main.rs
+
+C:\Users\yoon1\MIRU RUST\git_hello\target\debug\deps\git_hello.d: src\main.rs
+
+src\main.rs:
---
+++ MIRU RUST/git_hello/target/debug/git_hello.d
@@ -0,0 +1,1 @@
+C:\Users\yoon1\MIRU\ RUST\git_hello\target\debug\git_hello.exe: C:\Users\yoon1\MIRU\ RUST\git_hello\src\main.rs
