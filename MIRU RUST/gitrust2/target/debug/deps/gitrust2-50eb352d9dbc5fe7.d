7a22f6eb9ed5049f3bd8dbe4030f411501103419
yoon57942 <yoon57942@hanyang.ac.kr>
2023-11-13 19:51:27 +0900

    commit2

---
+++ MIRU RUST/gitrust2/target/debug/deps/gitrust2-50eb352d9dbc5fe7.d
@@ -0,0 +1,5 @@
+C:\Users\yoon1\MIRU RUST\gitrust2\target\debug\deps\gitrust2-50eb352d9dbc5fe7.rmeta: src\main.rs
+
+C:\Users\yoon1\MIRU RUST\gitrust2\target\debug\deps\gitrust2-50eb352d9dbc5fe7.d: src\main.rs
+
+src\main.rs:
---
+++ MIRU RUST/gitrust2/target/debug/deps/gitrust2-bb5414a8d39e8b5e.d
@@ -0,0 +1,5 @@
+C:\Users\yoon1\MIRU RUST\gitrust2\target\debug\deps\gitrust2-bb5414a8d39e8b5e.rmeta: src\main.rs
+
+C:\Users\yoon1\MIRU RUST\gitrust2\target\debug\deps\gitrust2-bb5414a8d39e8b5e.d: src\main.rs
+
+src\main.rs:
